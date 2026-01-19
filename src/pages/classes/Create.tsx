d75b839c2eae33325531e566962c9add748cb96e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-19 23:08:41 +0900

    feat: Add a card-based form structure to the class creation page.

--- src/pages/classes/Create.tsx
+++ src/pages/classes/Create.tsx
@@ -1,8 +1,11 @@
 import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
 import { CreateView } from '@/components/refine-ui/views/create-view'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
 import { useBack } from '@refinedev/core'
 import React from 'react'
+import { Form } from 'react-hook-form'
 
 const ClassesCreate = () => {
     const back = useBack()
@@ -16,6 +19,22 @@ const ClassesCreate = () => {
         <div className='intro-row'>
             <p>Provide the required information below to create a new class.</p>
             <Button onClick={back}>Go Back</Button>
+        </div>
+
+        <Separator />
+
+        <div className="my-4 flex items-center">
+            <Card className='class-form-card'>
+                <CardHeader className='relative z-10'>
+                    <CardTitle className='text-2xl pb-0 font-bold'>Fill out the form</CardTitle>
+                </CardHeader>
+
+                <Separator />
+
+                <CardContent className='mt-7'>
+                    {/* <Form></Form> */}
+                </CardContent>
+            </Card>
         </div>
     </CreateView>
   )
